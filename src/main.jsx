cb9bc755812a43731b7b6273d2f29b1f2d5c4824
Allukaparatha <ahjhrhah3@gmail.com>
2025-06-25 00:34:11 +0500

    complete

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <>
+  <h1 style={{textAlign: "center", color: "red"}}>Hello World!</h1>
+  <h2 style={{textAlign: "center", color: "green"}}>Hello React!!</h2>
+  </>
+)
+
+
